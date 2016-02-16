b9644acf20ecc9e13190cc9edee5a8d833b62559
minhcuong <cuong@techmaster.vn>
2016-02-16 11:33:35 +0700

    Initial Commit

---
+++ SwitchLandscapePortrait/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwitchLandscapePortrait
+//
+//  Created by Cuong Trinh on 2/16/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SwitchLandscapePortrait/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwitchLandscapePortrait
+//
+//  Created by Cuong Trinh on 2/16/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
